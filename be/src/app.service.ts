aeeb6c0ebdb03cbd3043d434dfc22581c7761a81
Afif Alfiano <afifalfiano2@gmail.com>
2021-09-13 07:49:12 +0700

    fix: integrate fe and be

--- be/src/app.service.ts
+++ be/src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async getHello(): Promise<any> {
+    return await {data: 'hello world'}
   }
 }
--- fe/src/app/app.component.ts
+++ fe/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'fe';
+export class AppComponent implements OnInit {
+  title;
+  baseUrl;
+  constructor(private httpService: HttpClient) {
+    this.baseUrl = environment.baseUrl;
+  }
+
+  ngOnInit() {
+    this.getData();
+  }
+
+  getData() {
+    const headers  = new HttpHeaders().set('Access-Control-Allow-Origin', '*');
+    return this.httpService.get(this.baseUrl + '/api/hello', {headers}).subscribe(res => {
+      console.log(res);
+    });
+  }
 }
