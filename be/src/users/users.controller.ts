8918227bcf864aa59e3f32766f4e54e4da5139b5
Afif Alfiano <afifalfiano2@gmail.com>
2021-09-13 10:21:25 +0700

    feat: upload files images and return image

--- be/src/users/users.controller.ts
+++ be/src/users/users.controller.ts
@@ -1,20 +1,43 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseInterceptors, UploadedFile, Res, Query } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { AnyFilesInterceptor, FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { extname, join } from 'path';
+import { of } from 'rxjs';
 
 @Controller('api/users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
+  @UseInterceptors(FileInterceptor('photo_profile',
+      {
+        storage: diskStorage({
+          destination: './photo-profile', 
+          filename: (req, file, cb) => {
+          return cb(null, `${file.originalname}`)
+        }
+        })
+      }
+    )
+    )
+  create(@Body() createUserDto: CreateUserDto, @UploadedFile() file: Express.Multer.File) {
+    console.log(file);
+    createUserDto.photo_profile = file.originalname;
     return this.usersService.create(createUserDto);
   }
 
   @Get()
   findAll() {
     return this.usersService.findAll();
+  }
+
+  @Get(':id/profile')
+  profile(@Query('filename') filename, @Res() res) {
+    console.log(filename);
+    return res.sendFile(join(process.cwd(), 'photo-profile/' + filename));
   }
 
   @Get(':id')
--- be/src/users/users.module.ts
+++ be/src/users/users.module.ts
@@ -3,9 +3,12 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { MulterModule } from '@nestjs/platform-express';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]), MulterModule.register({
+    dest: './photo-profile',
+  })],
   controllers: [UsersController],
   providers: [UsersService]
 })
