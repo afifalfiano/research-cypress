2e3e353d13237dc6d2317d1442a3b6d5edec3e71
Afif Alfiano <afifalfiano2@gmail.com>
2021-09-21 09:15:04 +0700

    be: remove limit throttle

--- be/src/app.module.ts
+++ be/src/app.module.ts
@@ -6,22 +6,16 @@ import { UsersModule } from './users/users.module';
 import { CompanyModule } from './company/company.module';
 import { AuthModule } from './auth/auth.module';
 import configSqlite from 'configorm';
-import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
-import { APP_GUARD } from '@nestjs/core';
 import { ConfigModule } from '@nestjs/config';
 
 
 @Module({
   imports: [
-    ThrottlerModule.forRoot({
-      ttl: 60,
-      limit: 10,
-    }),
     ConfigModule.forRoot({
       isGlobal: true,
     }),
     TypeOrmModule.forRoot(configSqlite), UsersModule, CompanyModule, AuthModule],
   controllers: [AppController],
-  providers: [AppService, { provide: APP_GUARD, useClass: ThrottlerGuard },],
+  providers: [AppService],
 })
 export class AppModule {}
