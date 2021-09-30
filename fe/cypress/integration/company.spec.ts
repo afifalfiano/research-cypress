0de28ae2894b158aca50db4818039cfa4873f58e
Afif Alfiano <afifalfiano2@gmail.com>
2021-09-30 21:31:02 +0700

    fe: comment test

--- fe/cypress/integration/company.spec.ts
+++ fe/cypress/integration/company.spec.ts
@@ -2,23 +2,21 @@ import { environment } from 'src/environments/environment';
 
 describe('Company Test', () => {
     beforeEach(() => {
-        cy.login('afifalfiano2@gmail.com', 'Admin12345');
-        cy.get('h1').should('contain.text', 'Selamat Datang');
-        const token = Cypress.env('login');
-        if (token !== undefined) {
-            // tslint:disable-next-line:max-line-length
-            cy.request({method: 'GET', url: environment.baseUrl + '/api/company', body: {}, headers: {Authorization: `bearer ${ token.access_token }`}}).as('company-list');
-            cy.get('@company-list').should((response) => {
-                // tslint:disable-next-line:no-string-literal
-                expect(response['status']).to.equal(200);
-            });
-        }
-        cy.get('a').contains('Companies').click();
-        cy.wait(2000);
+        // cy.login('afifalfiano2@gmail.com', 'Admin12345');
+        // cy.get('h1').should('contain.text', 'Selamat Datang');
+        // const token = Cypress.env('login');
+        // if (token !== undefined) {
+        //     cy.request({method: 'GET', url: environment.baseUrl + '/api/company', body: {}, headers: {Authorization: `bearer ${ token.access_token }`}}).as('company-list');
+        //     cy.get('@company-list').should((response) => {
+        //         expect(response['status']).to.equal(200);
+        //     });
+        // }
+        // cy.get('a').contains('Companies').click();
+        // cy.wait(2000);
     });
 
     afterEach(() => {
-        cy.wait(3000);
-        cy.logout('afifalfiano2@gmail.com');
+        // cy.wait(3000);
+        // cy.logout('afifalfiano2@gmail.com');
     });
   });
--- fe/cypress/integration/users.spec.ts
+++ fe/cypress/integration/users.spec.ts
@@ -2,19 +2,17 @@ import { environment } from 'src/environments/environment';
 
 describe('Users Test', () => {
     beforeEach(() => {
-        cy.login('afifalfiano2@gmail.com', 'Admin12345');
-        cy.get('h1').should('contain.text', 'Selamat Datang');
-        const token = Cypress.env('login');
-        if (token !== undefined) {
-            // tslint:disable-next-line:max-line-length
-            cy.request({method: 'GET', url: environment.baseUrl + '/api/users', body: {}, headers: {Authorization: `bearer ${ token.access_token }`}}).as('user-list');
-            cy.get('@user-list').should((response) => {
-                // tslint:disable-next-line:no-string-literal
-                expect(response['status']).to.equal(200);
-            });
-        }
-        cy.get('a').contains('Users').click();
-        cy.wait(2000);
+        // cy.login('afifalfiano2@gmail.com', 'Admin12345');
+        // cy.get('h1').should('contain.text', 'Selamat Datang');
+        // const token = Cypress.env('login');
+        // if (token !== undefined) {
+        //     cy.request({method: 'GET', url: environment.baseUrl + '/api/users', body: {}, headers: {Authorization: `bearer ${ token.access_token }`}}).as('user-list');
+        //     cy.get('@user-list').should((response) => {
+        //         expect(response['status']).to.equal(200);
+        //     });
+        // }
+        // cy.get('a').contains('Users').click();
+        // cy.wait(2000);
     });
 
     it('Create new user', () => {
@@ -33,7 +31,7 @@ describe('Users Test', () => {
     });
 
     afterEach(() => {
-        cy.wait(3000);
-        cy.logout('afifalfiano2@gmail.com');
+        // cy.wait(3000);
+        // cy.logout('afifalfiano2@gmail.com');
     });
   });
